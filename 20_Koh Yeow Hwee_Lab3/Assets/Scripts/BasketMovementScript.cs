205ef08878246a8354575137d2e906ca739cb702
YeowHwee <yeowhwee13@gmail.com>
2022-01-13 10:29:18 +0800

    Collision check
    Collision for items

--- 20_Koh Yeow Hwee_Lab3/Assets/Scripts/BasketMovementScript.cs
+++ 20_Koh Yeow Hwee_Lab3/Assets/Scripts/BasketMovementScript.cs
@@ -20,11 +20,24 @@ public class BasketMovementScript : MonoBehaviour
       float horizontalInput = Input.GetAxis("Horizontal");
       transform.position = transform.position + new Vector3(horizontalInput * speed * Time.deltaTime, 0, 0);
 
-        Vector2 pos = transform.position;
-        pos.x = Mathf.Clamp(pos.x, -maxX, maxX);
-        transform.position = pos;
+      //Restrict player from moving out of the screen
+      Vector2 pos = transform.position;
+      pos.x = Mathf.Clamp(pos.x, -maxX, maxX);
+      transform.position = pos;
     }
 
-    
+
+    //Collision for Healthy and Unhealthy items
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Healthy"))
+        {
+            Destroy(collision.gameObject);
+        }
+        else if(collision.gameObject.CompareTag("Unhealthy"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
 
 }
